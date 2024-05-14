f0e9f66cda37df91356aa6bf234753a4ec8f0b0b
reblapointe <reblapointe@gmail.com>
2024-05-14 08:45:19 -0400

    Solution date

--- PersonnageAuHasard/Program.cs
+++ PersonnageAuHasard/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace PersonnageAuHasard
 {
@@ -11,6 +12,21 @@ namespace PersonnageAuHasard
             string personnage3 = ChoisirPersonnage("troisième");
 
             Console.WriteLine($"Vos trois personnages sont {personnage1}, {personnage2} et {personnage3}");
+
+            // Solution date
+            Console.WriteLine("Entrez un jour");
+            int.TryParse(Console.ReadLine() ?? string.Empty, out int jour);
+            Console.WriteLine("Entrez un mois");
+            int.TryParse(Console.ReadLine() ?? string.Empty, out int mois);
+            Console.WriteLine("Entrez une annee");
+            int.TryParse(Console.ReadLine() ?? string.Empty, out int annee);
+
+            bool dateValide = DateValide(jour, mois, annee);
+            Console.WriteLine($"{jour}/{mois}/{annee} ");
+            if (dateValide)
+                Console.WriteLine("est valide.");
+            else
+                Console.WriteLine("n'est pas valide.");
         }
 
         /// <summary>
@@ -50,6 +66,56 @@ namespace PersonnageAuHasard
                 }
             } while (!personnageValide);
             return personnage;
+        }
+
+
+        /// <summary>
+        /// Détermine si l'année est bissextile ou non. Une année est bissextile si elle est divisible par
+        /// 4 mais pas par 100, ou si elle est divisible par 400.
+        /// </summary>
+        /// <param name="annee">l'année</param>
+        /// <returns>Vrai si année est bissextile, faux sinon.</returns>
+        public static bool EstBissextile(int annee)
+        {
+            return annee % 4 == 0 && annee % 100 != 0 || annee % 400 == 0;
+        }
+
+        /// <summary>
+        /// Retourne le nombre de jour dans un mois
+        /// </summary>
+        /// <param name="mois">Le mois</param>
+        /// <param name="annee">L'année</param>
+        /// <returns>28, 29, 30 ou 31, dépendemment du mois et de l'année. Si le mois est invalide, la fonction retourne 31.</returns>
+        public static int NbJoursDansMois(int mois, int annee)
+        {
+            switch (mois)
+            {
+                case 2:
+                    if (EstBissextile(annee))
+                        return 29;
+                    else
+                        return 28;
+                case 4:
+                case 6:
+                case 9:
+                case 11:
+                    return 30;
+                default:
+                    return 31;
+            }
+        }
+
+        /// <summary>
+        /// Détermine si le trio jour/mois/année forme une date valide selon le calendrier.
+        /// </summary>
+        /// <param name="jour">Le jour</param>
+        /// <param name="mois">Le mois</param>
+        /// <param name="annee">L'année</param>
+        /// <returns>Vrai si le trio forme une date valide, faux sinon</returns>
+        public static bool DateValide(int jour, int mois, int annee)
+        {
+            return 1 <= mois && mois <= 12
+                && 1 <= jour && jour <= NbJoursDansMois(mois, annee);
         }
     }
 }
