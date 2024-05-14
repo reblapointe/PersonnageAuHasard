be7cafc75698c09dfc8808fe0bdfe7472c6357b5
reblapointe <reblapointe@gmail.com>
2024-05-14 08:29:22 -0400

    Solution

--- PersonnageAuHasard/Program.cs
+++ PersonnageAuHasard/Program.cs
@@ -6,100 +6,45 @@ namespace PersonnageAuHasard
     {
         public static void Main(string[] _)
         {
-            string personnage1;
-            bool personnage1Valide;
-            do
-            {
-                personnage1Valide = true;
-                Console.WriteLine("Quel est le type du premier personnage? (R)obot, (S)orcière ou (Z)ombie?");
-                personnage1 = Console.ReadLine() ?? string.Empty;
-                personnage1 = personnage1.ToUpper().Trim();
-                switch (personnage1)
-                {
-                    case "R":
-                    case "ROBOT":
-                        personnage1 = "Robot";
-                        break;
-                    case "Z":
-                    case "ZOMBIE":
-                        personnage1 = "Zombie";
-                        break;
-                    case "S":
-                    case "SORCIÈRE":
-                    case "SORCIERE":
-                        personnage1 = "Sorcière";
-                        break;
-                    default:
-                        Console.WriteLine(personnage1 + " n'est pas un personnage valide.");
-                        personnage1Valide = false;
-                        break;
-                }
-            } while (!personnage1Valide);
-
-
-
-            string personnage2;
-            bool personnage2Valide;
-            do
-            {
-                personnage2Valide = true;
-                Console.WriteLine("Quel est le type du deuxième personnage? (R)obot, (S)orcière ou (Z)ombie?");
-                personnage2 = Console.ReadLine() ?? string.Empty;
-                personnage2 = personnage2.ToUpper().Trim();
-                switch (personnage2)
-                {
-                    case "R":
-                    case "ROBOT":
-                        personnage2 = "Robot";
-                        break;
-                    case "Z":
-                    case "ZOMBIE":
-                        personnage2 = "Zombie";
-                        break;
-                    case "S":
-                    case "SORCIÈRE":
-                    case "SORCIERE":
-                        personnage2 = "Sorcière";
-                        break;
-                    default:
-                        Console.WriteLine(personnage2 + " n'est pas un personnage valide.");
-                        personnage2Valide = false;
-                        break;
-                }
-            } while (!personnage2Valide);
-
-
-            string personnage3;
-            bool personnage3Valide;
-            do
-            {
-                personnage3Valide = true;
-                Console.WriteLine("Quel est le type du troisième personnage? (R)obot, (S)orcière ou (Z)ombie?");
-                personnage3 = Console.ReadLine() ?? string.Empty;
-                personnage3 = personnage3.ToUpper().Trim();
-                switch (personnage3)
-                {
-                    case "R":
-                    case "ROBOT":
-                        personnage3 = "Robot";
-                        break;
-                    case "Z":
-                    case "ZOMBIE":
-                        personnage3 = "Zombie";
-                        break;
-                    case "S":
-                    case "SORCIÈRE":
-                    case "SORCIERE":
-                        personnage3 = "Sorcière";
-                        break;
-                    default:
-                        Console.WriteLine(personnage3 + " n'est pas un personnage valide.");
-                        personnage3Valide = false;
-                        break;
-                }
-            } while (!personnage3Valide);
+            string personnage1 = ChoisirPersonnage("premier");
+            string personnage2 = ChoisirPersonnage("deuxième");
+            string personnage3 = ChoisirPersonnage("troisième");
 
             Console.WriteLine($"Vos trois personnages sont {personnage1}, {personnage2} et {personnage3}");
+        }
+
+        private static string ChoisirPersonnage(string position)
+        {
+            string personnage;
+            bool personnageValide;
+            do
+            {
+                personnageValide = true;
+                Console.WriteLine("Quel est le type du " + position + " personnage? (R)obot, (S)orcière ou (Z)ombie?");
+                personnage = Console.ReadLine() ?? string.Empty;
+                personnage = personnage.ToUpper().Trim();
+                switch (personnage)
+                {
+                    case "R":
+                    case "ROBOT":
+                        personnage = "Robot";
+                        break;
+                    case "Z":
+                    case "ZOMBIE":
+                        personnage = "Zombie";
+                        break;
+                    case "S":
+                    case "SORCIÈRE":
+                    case "SORCIERE":
+                        personnage = "Sorcière";
+                        break;
+                    default:
+                        Console.WriteLine(personnage + " n'est pas un personnage valide.");
+                        personnageValide = false;
+                        break;
+                }
+            } while (!personnageValide);
+            return personnage;
         }
     }
 }
